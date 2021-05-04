c043fc741480d9ffa8a3994f2a6439606d447768
Putta Prakash <putta.prakash@puprakash@merklinc.com>
2021-05-04 15:54:05 +0530

    Completed Chapter 2

---
+++ 02_01/module-demo.js
@@ -0,0 +1,3 @@
+const mymodule = require('./my-module');
+
+console.log(mymodule.myText);
---
+++ 02_03/demo.js
@@ -0,0 +1,4 @@
+var _ = require('lodash');
+var nodeman = require('nodeman')
+
+console.log(_.random(1,100))
