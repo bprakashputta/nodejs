90ff8e9328539c4b64e4f8d04ebc60fa98d594c4
Putta Prakash <putta.prakash@puprakash@merklinc.com>
2021-05-24 08:52:47 +0530

    Added Module5

--- 05_01/server.js
+++ 05_01/server.js
@@ -1,8 +1,7 @@
-var express = require('express')
+var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname));
-
-var server = app.listen(3000,(err,data)=>{
-    console.log("Server is running on port",server.address().port)
+app.use(express(__dirname));
+var server = app.listen(3000, () => {
+    console.log("Server is running on port", server.address().port);
 })
